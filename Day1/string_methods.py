9e8f93ca56e907cae06f6b0a5a829baead3d73a3
salman <sf991804@gmail.com>
2024-03-05 01:23:03 +0500

    Added Python Strings Methods

---
+++ Day1/string_methods.py
@@ -0,0 +1,26 @@
+b1 ='sf39'
+print(b1.isalnum() )
+
+b2 ='letmesee'
+print(b2.isalpha() )
+
+b3 ='156'
+b3.isdecimal()
+
+b4 ='132'
+b4.isdigit() 
+
+b5='jhon_'
+b5.isidentifier() 
+
+b5 = 'jHon'
+b5.isnumeric() 
+
+b6 =' '
+b6.isspace()
+
+b7 = 'Each Word Contain Title'
+b7.istitle() 
+
+b8='ALLUPPER CASE'
+b8.isupper() 
